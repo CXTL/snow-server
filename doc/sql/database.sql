74f678e97b0ddf4635e085f823b6e3c17fafe7d6
hb_cxtl@163.com <Cxt1997116>
2022-02-05 19:01:48 +0800

    新增NACOS / DB脚本

---
+++ doc/sql/database.sql
@@ -0,0 +1,16 @@
+/*
+* MySQL8.x版本
+*/
+
+-- ----------------------------
+-- 系统管理数据库
+-- ----------------------------
+CREATE DATABASE IF NOT EXISTS snow_system DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
+-- ----------------------------
+-- NACOS
+-- ----------------------------
+CREATE DATABASE IF NOT EXISTS nacos_config DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
+
+
+
+
