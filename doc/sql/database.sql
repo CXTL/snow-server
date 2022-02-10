f7d8a87e6d7afa0017d6a04c8c1878ee7f0b1c5b
xt <hb_cxtl@163.com>
2022-02-10 18:00:13 +0800

    新增 snow_meta.sql 语句

--- doc/sql/database.sql
+++ doc/sql/database.sql
@@ -7,6 +7,10 @@
 -- ----------------------------
 CREATE DATABASE IF NOT EXISTS snow_system DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
 -- ----------------------------
+-- 数据源管理数据库
+-- ----------------------------
+CREATE DATABASE IF NOT EXISTS snow_meta DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
+-- ----------------------------
 -- NACOS
 -- ----------------------------
 CREATE DATABASE IF NOT EXISTS nacos_config DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
---
+++ doc/sql/snow_meta.sql
@@ -0,0 +1,74 @@
+/*
+* 系统管理表
+* MySQL8.x版本
+*/
+use snow_meta;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+-- ----------------------------
+-- Table structure for meta_datasource_info
+-- ----------------------------
+DROP TABLE IF EXISTS `meta_datasource_info`;
+CREATE TABLE `meta_datasource_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `datasource_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '数据源名称',
+  `database_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '数据库名称',
+  `schema_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'schema名称',
+  `driver_class_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '数据驱动名称',
+  `url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '连接数据库地址',
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `ip` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'ip地址',
+  `port` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '端口',
+  `status` tinyint(1) NULL DEFAULT 1 COMMENT '数据源状态：1-正常 0-禁用',
+  `type` tinyint(1) NULL DEFAULT 1 COMMENT '数据源类型 1:mysql 2:oracle 3:pgsql 4:hive 5:hbase',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '逻辑删除标识：0-未删除；1-已删除',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `description` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '数据源信息表' ROW_FORMAT = DYNAMIC;
+
+
+
+DROP TABLE IF EXISTS `meta_table_info`;
+CREATE TABLE `meta_table_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `datasource_id` bigint NOT NULL COMMENT '数据源ID',
+  `database_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '数据库名称',
+  `schema_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'schema名称',
+  `table_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '数据表名称',
+  `table_name_chinese` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '数据表中文名称',
+  `type` tinyint(1) NULL DEFAULT 0 COMMENT '数据源类型  0:表 1:视图，2:SQL 3:代码表',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '逻辑删除标识：0-未删除；1-已删除',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `description` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '数据表信息表' ROW_FORMAT = DYNAMIC;
+
+
+
+
+DROP TABLE IF EXISTS `meta_field_info`;
+CREATE TABLE `meta_field_info`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `table_id` bigint NOT NULL COMMENT '数据表ID',
+  `field_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '字段名称',
+  `field_name_chinese` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '字段中文名称',
+  `type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '字段类型',
+  `field_length` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '字段长度',
+  `field_decimal` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '小数点位数',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '逻辑删除标识：0-未删除；1-已删除',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `description` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '数据字段信息信息表' ROW_FORMAT = DYNAMIC;
+
+
+
+SET FOREIGN_KEY_CHECKS = 1;
